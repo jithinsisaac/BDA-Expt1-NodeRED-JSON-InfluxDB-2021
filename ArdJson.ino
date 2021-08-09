3f7b85f41f427e6b888cdab21293d8018a07ca17
Jithin Saji Isaac <43294464+jithinsisaac@users.noreply.github.com>
2021-08-09 13:20:53 +0530

    Update ArdJson.ino

--- ArdJson.ino
+++ ArdJson.ino
@@ -1,23 +1,32 @@
 #include "ArduinoJson.h"
-
 void setup() 
 {
   Serial.begin(115200);
 }
 
+      float temperature=0;
+      float humidity=0;
+      float pressure = 1000;
+      
 void loop() 
 {
-
       StaticJsonDocument<200> jsonDoc; 
       
-      float temperature=30.5;
-      float humidity=80;
-
       jsonDoc["temp"] = temperature;
       jsonDoc["humi"] = humidity;
+      jsonDoc["pressure"] = pressure;
    
       serializeJsonPretty(jsonDoc, Serial);
       Serial.println(); 
 
+      temperature++;
+      humidity++;
+      
+      if(temperature ==40)
+     {   temperature =0;}
+        
+      if(humidity ==100)
+        {humidity =0;}
+        
       delay(2000);
 }
